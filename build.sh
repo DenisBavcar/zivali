0e996f6290ae8c6cecd9f17775b37c9e6ef62015
DenisBavcar <denis.bavcar@gmail.com>
2023-09-20 11:05:17 +0200

    render internetna stran

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry install
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+# Make sure the script is executable before checking it into Git: chmod a+x build.sh
+# from StackOverflow: If the concern is the executable bit, you can run git update-index --chm od=+x run.sh after
+# adding it to make it executable on platforms that support that concept.
